047045811c6ed43595b272c83224204f9532e2e4
akat32 <baduk32127@gmail.com>
2017-12-04 00:43:34 +0900

    asd

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
 app.use(CORS);
 require('./mongo');
+
 var port = process.env.PORT || 4000;
 var main = require('./routes/main')(express.Router(), fs);
 var quiz = require('./routes/quiz')(express.Router(), fs);
@@ -21,11 +22,23 @@ app.use('/main', main);
 app.use('/quiz', quiz);
 app.use('/transe', transe);
 app.use('/word', word);
-
 app.get('/',(req,res)=>{
   res.redirect('/main');
-});
-
+})
+.post('/word/change', async(req,res)=>{
+  String.prototype.replaceAll = function(org, dest) {
+    return this.split(org).join(dest);
+  }
+  var result = await Words.find().sort({word : 1});
+  var str = req.body.str;
+  var ss;
+  for(i=0;;i++){
+    if(!result[i]) break;
+    ss = str.replaceAll(result[i].word,result[i].change);
+  }
+  console.log(ss);
+  res.status(200).send(ss);
+})
 
 app.listen(3111, ()=>{
   console.log('Server portin on 3111')
--- routes/word.js
+++ routes/word.js
@@ -1,6 +1,3 @@
-String.prototype.replaceAll = function(org, dest) {
-    return this.split(org).join(dest);
-}
 module.exports = (router,fs,Words)=>{
   router.post('/save', async (req,res)=>{
     var new_word = new Words(req.body);
@@ -13,17 +10,6 @@ module.exports = (router,fs,Words)=>{
     let result = await Words.find({word : rquery});
     if(!result) return res.status(404).json({message : "Word not find!"});
     else return res.status(200).json(result);
-  })
-  .post('/change', async(req,res)=>{
-    var result = await Words.find().sort({word : 1});
-    var str = req.body.str;
-    var ss;
-    for(i=0;;i++){
-      if(!result[i]) break;
-      ss = str.replaceAll(result[i].word,result[i].change);
-    }
-    console.log(ss);
-    res.status(200).send(ss);
   })
   .post('/chk', async (req,res)=>{
     var result = await Words.find().sort({_id : 1});
